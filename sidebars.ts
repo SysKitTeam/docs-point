b031869e676b21149cb716fc680b335918c1e6da
imesaric <igor.mesaric.w@outlook.com>
2026-01-02 13:59:19 +0100

    added 20260108 Cloud RN

--- sidebars.ts
+++ sidebars.ts
@@ -26,6 +26,7 @@ const sidebars: SidebarsConfig = {
             id: 'releases/cloud/README',
           },
           items: [
+            'releases/cloud/point-2026-01-08-release-note',
             'releases/cloud/point-2025-12-16-release-note',
             'releases/cloud/point-2025-12-09-release-note',
             'releases/cloud/point-2025-12-02-release-note',
