f98bcb9773654425f985a2973c96fe3f52e95c88
imesaric <igor.mesaric.w@outlook.com>
2026-05-08 13:41:23 +0200

    Revert "add restrict site creation for apps documentation and update sidebar"
    This reverts commit 0a265bd141467979586b7c42cbca450018bef063.

--- sidebars.ts
+++ sidebars.ts
@@ -550,8 +550,7 @@ const sidebars: SidebarsConfig = {
         'configuration/configure-point-for-storage-management',
         'configuration/custom-views',
         'configuration/audit-log-for-settings',
-        'configuration/manage-connection',
-        'configuration/restrict-site-creation-for-apps', 
+        'configuration/manage-connection', 
       ],
     },
     {
